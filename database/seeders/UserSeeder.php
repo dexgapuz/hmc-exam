76d9b8ff100d50f98b46dcca882fb3a81daa984c
Dexter Gapuz <dxgapuz@gmail.com>
2024-10-04 01:50:54 +0800

    added admin account and changed readme for documentation

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -5,6 +5,8 @@ namespace Database\Seeders;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 
 class UserSeeder extends Seeder
 {
@@ -13,6 +15,14 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
+        User::create([
+            'name' => 'admin',
+            'email' => 'admin@email.com',
+            'password' => Hash::make('Qwerty123!'),
+            'email_verified_at' => now(),
+            'remember_token' => Str::random(10)
+        ]);
+
         User::factory()
             ->count(5)
             ->create();
